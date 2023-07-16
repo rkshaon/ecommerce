baced0658170c8dcc4114eb35b3d03169db3df72
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2023-07-16 18:38:32 +0600

    frontend: home page setup

---
+++ frontend/src/components/HomeComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="">
+    <h1>Home Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeComponent',
+  props: {}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
