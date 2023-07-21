605266f2d582acc0b81ba028b1c58efe0f3da6e6
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2023-07-21 19:02:22 +0600

    frontend: footer - wip and fontawesome install, setup - done

--- frontend/src/components/common/FooterComponent.vue
+++ frontend/src/components/common/FooterComponent.vue
@@ -1,18 +1,30 @@
 <template>
-    <div class="container-fluid" style="background-color: #242526; color: aliceblue;">
+    <div class="container-fluid" style="background-color: #242526; color: aliceblue; height: 500px;">
         <div class="row">
             <div class="col-2"></div>
-            <div class="col-3" style="background-color: #151616;">
-                Cell
+            <div class="col-3 pt-5" style="background-color: #151616;">
+                <div>
+                    <font-awesome-icon :icon="['fas', 'phone']" size="2xl"/>
+                    <p class="text-center">
+                        <a href="tel:+8801521205142" style="text-decoration: none;" class="text-white">+880 1521 20 51 42</a>
+                    </p>
+                </div>
             </div>
-            <div class="col-3">
-                    Cell
+            <div class="col-3 pt-5" style="background-color: #111;">
+                <div>
+                    <font-awesome-icon :icon="['fas', 'envelope']" size="2xl" />
+                    <p class="text-center">
+                        <a href="mailto:rkshaon.ist@gmail.com" style="text-decoration: none;" class="text-white">rkshaon.ist@gmail.com</a>
+                    </p>
+                </div>
             </div>
-            <div class="col-3">
-                Cell
+            <div class="col-3 pt-5" style="background-color: #151616;">
+                <div>
+                    <font-awesome-icon :icon="['fas', 'map-pin']" size="2xl" />
+                    <p class="text-center">Floor # 3, House # 11/B, Road # 30, Gulshan 1, Dhaka.</p>
+                </div>
             </div>
         </div>
-        Hello
     </div>
 </template>
 
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,23 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
 
-createApp(App).mount('#app')
+/* import specific icons */
+import { faEnvelope, faPhone, faMapPin } from '@fortawesome/free-solid-svg-icons'
+// import { faAddressBook } from '@fortawesome/free-regular-svg-icons'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+
+/* add icons to the library */
+library.add(faEnvelope, faPhone, faMapPin, faTwitter)
+
+const app = createApp(App);
+
+app.component('font-awesome-icon', FontAwesomeIcon);
+app.mount("#app");
