f22ae4fd89d4508305ad540c8fbbb4a1f64cdfa3
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2023-07-21 19:11:25 +0600

    frontend: footer - wip

--- frontend/src/components/common/FooterComponent.vue
+++ frontend/src/components/common/FooterComponent.vue
@@ -4,24 +4,26 @@
             <div class="col-2"></div>
             <div class="col-3 pt-5" style="background-color: #151616;">
                 <div>
-                    <font-awesome-icon :icon="['fas', 'phone']" size="2xl"/>
+                    <font-awesome-icon :icon="['fas', 'phone']" size="2xl" style="background-color: #39c7da; border-radius: 50%; padding: 15px;"/>
                     <p class="text-center">
                         <a href="tel:+8801521205142" style="text-decoration: none;" class="text-white">+880 1521 20 51 42</a>
                     </p>
                 </div>
             </div>
-            <div class="col-3 pt-5" style="background-color: #111;">
+            <div class="col-3 pt-5 footer-item">
                 <div>
-                    <font-awesome-icon :icon="['fas', 'envelope']" size="2xl" />
+                    <font-awesome-icon :icon="['fas', 'envelope']" size="2xl" style="background-color: #39c7da; border-radius: 50%; padding: 15px;"/>
                     <p class="text-center">
                         <a href="mailto:rkshaon.ist@gmail.com" style="text-decoration: none;" class="text-white">rkshaon.ist@gmail.com</a>
                     </p>
                 </div>
             </div>
-            <div class="col-3 pt-5" style="background-color: #151616;">
+            <div class="col-3 pt-5 footer-item" style="background-color: #151616;">
                 <div>
-                    <font-awesome-icon :icon="['fas', 'map-pin']" size="2xl" />
-                    <p class="text-center">Floor # 3, House # 11/B, Road # 30, Gulshan 1, Dhaka.</p>
+                    <font-awesome-icon :icon="['fas', 'map-pin']" size="2xl" style="background-color: #39c7da; border-radius: 50%; padding: 15px 20px;"/>
+                    <p class="text-center">
+                        <a href="https://goo.gl/maps/uvQvRnu3ucRDuamw9" target="_blank" style="text-decoration: none;" class="text-white">Floor # 3, House # 11/B, Road # 30, Gulshan 1, Dhaka.</a>
+                    </p>
                 </div>
             </div>
         </div>
@@ -33,3 +35,9 @@ export default {
     name: 'FooterComponent'
 }
 </script>
+
+<style>
+.footer-item::hover {
+    background-color: #151616
+}
+</style>
