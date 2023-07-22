be8573e8a9ae6c02366f1e25b7d0f1ed26f3f05d
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2023-07-22 23:53:11 +0600

    backend: category model - done

--- backend/category_api/models.py
+++ backend/category_api/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 
 class Category(models.Model):
     title = models.CharField(max_length=255, blank=False, null=False)
-    category_slug = models.SlugField(unique=True, blank=False, null=False)
+    category_slug = models.SlugField(unique=True, blank=True, null=True)
     short_title = models.CharField(max_length=255, blank=True, null=True)
     icon = models.CharField(max_length=255, blank=True, null=True)
     description = models.TextField(null=True, blank=True)
@@ -14,10 +14,15 @@ class Category(models.Model):
     is_active = models.BooleanField(default=True)
     is_deleted = models.BooleanField(default=False)
     added_by = models.ForeignKey(User, on_delete=models.CASCADE)
-    
+    added_date_time = models.DateTimeField(auto_now_add=True)
+
 
     def save(self, *args, **kwargs):
         if not self.category_slug:
             self.category_slug = slugify(self.title)
         
         super().save(*args, **kwargs)
+    
+
+    def __str__(self):
+        return f"{self.id}: {self.title}"
