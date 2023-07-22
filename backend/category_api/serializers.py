247fda7b75cc5fa8d6ae5e33d3b227e677032192
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2023-07-23 01:36:00 +0600

    backend: category - serializer updated - called self serializer for child category

--- backend/category_api/serializers.py
+++ backend/category_api/serializers.py
@@ -5,7 +5,22 @@ from category_api.models import Category
 
 
 class CategorySerializer(serializers.ModelSerializer):
+    """Serializer for the Category model."""
     class Meta:
         model = Category
         fields = '__all__'
+    
+
+    def get_serializer(self, instance):
+        """Returns a serializer instance for the given instance."""
+        return self.__class__(instance=instance)
+    
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
         
+        representation['children'] = [
+            self.get_serializer(child).data for child in instance.children.all()
+        ]
+        return representation
+    
