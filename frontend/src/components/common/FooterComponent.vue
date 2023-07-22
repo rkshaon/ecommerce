e7f4ad684586b3918007e8d5bf4d551db3154f6d
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2023-07-22 16:14:31 +0600

    frontend: footer - updated

--- frontend/src/components/common/FooterComponent.vue
+++ frontend/src/components/common/FooterComponent.vue
@@ -3,28 +3,34 @@
         <div class="container">
             <div class="row">
                 <div class="col-4 pt-5 pb-5" style="background-color: #151616;">
-                    <div>
-                        <font-awesome-icon :icon="['fas', 'phone']" size="2xl" style="background-color: #39c7da; border-radius: 50%; padding: 15px;"/>
-                        <p class="text-center">
-                            <a href="tel:+8801521205142" style="text-decoration: none;" class="text-white">+880 1521 20 51 42</a>
-                        </p>
-                    </div>
+                    <a href="tel:+8801521205142" style="text-decoration: none;" class="text-white">
+                        <div>
+                            <div class="text-center">
+                                <font-awesome-icon :icon="['fas', 'phone']" size="2xl" style="background-color: #39c7da; border-radius: 50%; padding: 15px;"/>
+                            </div>
+                            <p class="text-center pt-3">+880 1521 20 51 42</p>
+                        </div>
+                    </a>
                 </div>
                 <div class="col-4 pt-5 pb-5 footer-item">
-                    <div>
-                        <font-awesome-icon :icon="['fas', 'envelope']" size="2xl" style="background-color: #39c7da; border-radius: 50%; padding: 15px;"/>
-                        <p class="text-center">
-                            <a href="mailto:rkshaon.ist@gmail.com" style="text-decoration: none;" class="text-white">rkshaon.ist@gmail.com</a>
-                        </p>
-                    </div>
+                    <a href="mailto:rkshaon.ist@gmail.com" style="text-decoration: none;" class="text-white">
+                        <div>
+                            <div class="text-center">
+                                <font-awesome-icon :icon="['fas', 'envelope']" size="2xl" style="background-color: #39c7da; border-radius: 50%; padding: 15px;"/>
+                            </div>
+                            <p class="text-center pt-3">rkshaon.ist@gmail.com</p>
+                        </div>
+                    </a>
                 </div>
                 <div class="col-4 pt-5 pb-5 footer-item" style="background-color: #151616;">
-                    <div>
-                        <font-awesome-icon :icon="['fas', 'map-pin']" size="2xl" style="background-color: #39c7da; border-radius: 50%; padding: 15px 20px;"/>
-                        <p class="text-center">
-                            <a href="https://goo.gl/maps/uvQvRnu3ucRDuamw9" target="_blank" style="text-decoration: none;" class="text-white">Floor # 3, House # 11/B, Road # 30, Gulshan 1, Dhaka.</a>
-                        </p>
-                    </div>
+                    <a href="https://goo.gl/maps/uvQvRnu3ucRDuamw9" target="_blank" style="text-decoration: none;" class="text-white">
+                        <div>
+                            <div class="text-center">
+                                <font-awesome-icon :icon="['fas', 'map-pin']" size="2xl" style="background-color: #39c7da; border-radius: 50%; padding: 15px 20px;"/>
+                            </div>
+                            <p class="text-center pt-3">Floor # 3, House # 11/B, Road # 30, Gulshan 1, Dhaka.</p>
+                        </div>
+                    </a>
                 </div>
             </div>
             <div class="row mt-5 pb-5">
