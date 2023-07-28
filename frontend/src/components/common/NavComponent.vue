08299d24c1994395763ecf33c1cffe5ffd873268
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2023-07-29 00:12:09 +0600

    frontend: dynamic navbar - done

--- frontend/src/components/common/NavComponent.vue
+++ frontend/src/components/common/NavComponent.vue
@@ -13,11 +13,11 @@
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container">
                 <ul class="navbar-nav">
-                    <li v-for="item in categories" :key="item.id" class="nav-item" :class="{ 'dropdown': item.children && item.children.length > 0 }">
-                        <a v-if="item.children && item.children.length > 0" class="nav-link dropdown-toggle" :href="'#' + item.category_slug" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{ item.title }}</a>
-                        <router-link v-else class="nav-link" :to="{ name: 'category', params: { slug: item.category_slug } }">{{ item.title }}</router-link>
-                        <ul v-if="item.children && item.children.length > 0" class="dropdown-menu" :aria-labelledby="'#' + item.category_slug">
-                            <li v-for="child in item.children" :key="child.id">
+                    <li v-for="category in categories" :key="category.id" class="nav-item" :class="{ 'dropdown': category.children && category.children.length > 0 }">
+                        <a v-if="category.children && category.children.length > 0" class="nav-link dropdown-toggle" :href="'#'" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{ category.title }}</a>
+                        <router-link v-else class="nav-link" :to="{ name: 'category', params: { slug: category.category_slug } }">{{ category.title }}</router-link>
+                        <ul v-if="category.children && category.children.length > 0" class="dropdown-menu" :aria-labelledby="'#' + category.category_slug">
+                            <li v-for="child in category.children" :key="child.id">
                                 <router-link class="dropdown-item" :to="{ name: 'category', params: { slug: child.category_slug } }">{{ child.title }}</router-link>
                             </li>
                         </ul>
