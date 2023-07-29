e642f5e1b277eb4a7d83cbf031da7fa17f30c246
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2023-07-29 19:36:54 +0600

    frontend: getting category to display in categorywise product grid view

--- frontend/src/components/CategoryWiseProductComponent.vue
+++ frontend/src/components/CategoryWiseProductComponent.vue
@@ -1,19 +1,32 @@
 <template>
   <div class="">
-    This is Category wise Product List Component
-    <ProductGridViewComponent />
+    <!-- {{ category.title }} -->
+    <ProductGridViewComponent :category="category" />
   </div>
 </template>
 
 <script>
 import ProductGridViewComponent from '@/components/utility/ProductGridViewComponent.vue';
+import { useRoute } from 'vue-router';
+import categoryStore from '@/store/category';
 
 export default {
   name: 'CategoryWiseProductComponent',
   props: {},
   components: {
     ProductGridViewComponent,
-  }
+  },
+  setup() {
+    const route = useRoute();
+    const slug = route.params.slug;
+    // console.log('Category Slug: ',slug);
+    const category = categoryStore.state.categories.find(category => category.category_slug === slug);
+    // console.log('Category: ', category);
+    return {
+      slug,
+      category,
+    }
+  },
 }
 </script>
 
--- frontend/src/components/utility/ProductGridViewComponent.vue
+++ frontend/src/components/utility/ProductGridViewComponent.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="container mt-5 mb-5">
-        <h1 class="text-center">Featured Products</h1>
+        <!-- <h1 class="text-center">{{ if category}} Products</h1> -->
+        <h1 v-if="category" class="text-center">{{ category.title }} Products</h1>
+        <h1 v-else class="text-center">Some unknown Products</h1>
         <p class="text-center">Here is some featured products.</p>
         <div class="row">
             <div class="col-3">
@@ -75,7 +77,13 @@ export default {
     name: 'ProductGridViewComponent',
     components: {
         ProductCardComponent,
-    }
+    },
+    props: {
+        category: {
+            type: Object,
+            required: true,
+        },
+    },
 }
 </script>
 
