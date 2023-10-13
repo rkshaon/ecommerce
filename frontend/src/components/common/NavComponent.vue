196bddd4e927be4af10526da3a33663772b76c08
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2023-10-13 18:57:43 +0600

    frontend: update navbar component - remove children realted dropdown

--- frontend/src/components/common/NavComponent.vue
+++ frontend/src/components/common/NavComponent.vue
@@ -13,14 +13,8 @@
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container">
                 <ul class="navbar-nav">
-                    <li v-for="category in categories" :key="category.id" class="nav-item" :class="{ 'dropdown': category.children && category.children.length > 0 }">
-                        <a v-if="category.children && category.children.length > 0" class="nav-link dropdown-toggle" :href="'#'" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{ category.title }}</a>
-                        <router-link v-else class="nav-link" :to="{ name: 'category', params: { slug: category.category_slug } }">{{ category.title }}</router-link>
-                        <ul v-if="category.children && category.children.length > 0" class="dropdown-menu" :aria-labelledby="'#' + category.category_slug">
-                            <li v-for="child in category.children" :key="child.id">
-                                <router-link class="dropdown-item" :to="{ name: 'category', params: { slug: child.category_slug } }">{{ child.title }}</router-link>
-                            </li>
-                        </ul>
+                    <li v-for="category in categories" :key="category.id" class="nav-item">
+                        <router-link class="nav-link" :to="{ name: 'category', params: { slug: category.category_slug } }">{{ category.title }}</router-link>
                     </li>
                 </ul>
             </div>
