c8656c68ef2d5f089205ba5acb3673c10dd4c26f
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2023-12-11 01:37:46 +0600

    frontend: categorywisecomponent - updated based on category id, fetch data

--- frontend/src/components/CategoryWiseProductComponent.vue
+++ frontend/src/components/CategoryWiseProductComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <h1>Category Wise Products</h1>
+    <h1>{{ categoryDetails.title }}</h1>
     <p>Category Slug: {{ $route.params.category_slug }} {{ $route.params.category_id }}</p>
   </div>
 </template>
@@ -9,8 +9,40 @@
 
 export default {
   name: 'CategoryWiseProductComponent',
+  data() {
+    return {
+      categoryDetails: {
+        'title': 'Category Title is coming soon.',
+      },
+      loading: true,
+    };
+  },
   props: {},
   components: {
+  },
+  mounted() { },
+  created() {
+    this.fetchCategoryDetailsData();
+  },
+  watch: {
+    // Watch for changes in route parameters
+    '$route.params.category_id': 'fetchCategoryDetailsData',
+  },
+  methods: {
+    async fetchCategoryDetailsData() {
+      let url = `http://127.0.0.1:8000/api/categories/${this.$route.params.category_id}`;
+      console.log(url);
+
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        this.categoryDetails = data.data;
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      } finally {
+        this.loading = false;
+      }
+    },
   },
 }
 </script>
