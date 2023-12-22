5ea947fe5fdaac2b23b2c14d9b0b8b20411a8f5d
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2023-12-23 01:18:19 +0600

    frontend: 404 page - update

--- frontend/src/components/NotFoundComponent.vue
+++ frontend/src/components/NotFoundComponent.vue
@@ -1,10 +1,15 @@
 <template>
     <div class="container">
-        <h1>404 - Not found!</h1>
-        <p>Sorry, the page you are looking does not exit.</p>
-        <router-link to="/">
-            <p>Go to Home</p>
-        </router-link>
+        <div id="background">
+            <div class="text-center text-white" style="padding-top: 250px;">
+                <p class="" style="font-size: 7em; font-weight: bolder;">404</p>
+                <p style="font-size: 4em; font-weight: bolder;">Not found!</p>                
+                <router-link to="/">
+                    <font-awesome-icon :icon="['fas', 'house']" class="text-white" style="font-size: 5em" />
+                </router-link>
+                <p style="font-size: 2em; font-weight: bold;">Sorry, the page you are looking does not exit.</p>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -12,5 +17,21 @@
 
 export default {
     name: 'NotFoundComponent',
+    data() {
+        return { }
+    },
+    computed: { },
 };
 </script>
+
+<style scoped>
+#background {
+  background: url("../assets/404.jpg") no-repeat center center
+    fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  height: 1000px;
+}
+</style>
