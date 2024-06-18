92c3f2d1359395fc2e0473b957376db50640279f
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-06-19 00:22:05 +0600

    rework: frontend - delete category modal - wip

--- frontend/src/components/admin/category/AdminCategoryListComponent.vue
+++ frontend/src/components/admin/category/AdminCategoryListComponent.vue
@@ -98,26 +98,7 @@
             </div>
         </div>
         <!-- Delete category modal -->
-        <div class="modal fade" id="deleteCategoryModal" tabindex="-1" aria-labelledby="deleteCategoryModalLabel"
-            aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="deleteCategoryModalLabel">Delete Category</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body text-start">
-                        <div class="mb-3">
-                            <label for="exampleInputText1" class="form-label">Are you sure?</label>
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-danger" @click="deleteCategory">Yes, delete</button>
-                    </div>
-                </div>
-            </div>
-        </div>
+        
         <!-- Add category button -->
         <button type="button" class="btn btn-primary btn-lg mb-2" data-bs-toggle="modal"
             data-bs-target="#addCategoryModal">
