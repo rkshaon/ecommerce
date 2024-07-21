9a62ec6af1411a56c28134c6c5392e474af81cde
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-07-21 23:59:05 +0600

    backend: flake8 compatible code

--- backend/category_api/serializers.py
+++ backend/category_api/serializers.py
@@ -3,26 +3,24 @@ from rest_framework import serializers
 from category_api.models import Category
 
 
-
 class CategorySerializer(serializers.ModelSerializer):
     """Serializer for the Category model."""
     icon = serializers.FileField(required=False)
-    
+
     class Meta:
         model = Category
         fields = '__all__'
-    
 
     # def get_serializer(self, instance):
     #     """Returns a serializer instance for the given instance."""
     #     return self.__class__(instance=instance)
-    
 
     # def to_representation(self, instance):
     #     representation = super().to_representation(instance)
-        
+
     #     representation['children'] = [
-    #         self.get_serializer(child).data for child in instance.children.all()
+    #         self.get_serializer(
+    #             child
+    #         ).data for child in instance.children.all()
     #     ]
     #     return representation
-    
--- backend/category_api/views.py
+++ backend/category_api/views.py
@@ -1,28 +1,23 @@
-from rest_framework.viewsets import ViewSet
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.decorators import permission_classes, authentication_classes
+from rest_framework.decorators import permission_classes
+from rest_framework.decorators import authentication_classes
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.pagination import PageNumberPagination
 from rest_framework import status
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
-from django.shortcuts import get_object_or_404
-
 import copy
 
 from category_api.models import Category
-from product_api.models import Product
 
 from category_api.serializers import CategorySerializer
-from product_api.serializers import ProductSerializer
-
 
 
 class CategoryView(APIView):
     serializer_class = CategorySerializer
-    
+
     @authentication_classes([SessionAuthentication, JWTAuthentication])
     @permission_classes([IsAuthenticated])
     def post(self, request, *args, **kwargs):
@@ -30,26 +25,27 @@ class CategoryView(APIView):
         request_data['added_by'] = request.user.id
 
         serializer = self.serializer_class(data=request_data)
-        
+
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        
+
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
 
     def get(self, request, *args, **kwargs):
         pk = kwargs.get('pk', None)
 
         if pk:
             try:
-                data = self.serializer_class(Category.objects.get(pk=pk)).data
+                data = self.serializer_class(
+                    Category.objects.get(pk=pk)
+                ).data
                 return Response(data)
             except Category.DoesNotExist:
                 return Response({
                     'error': 'Category does not exist.'
                 }, status=status.HTTP_404_NOT_FOUND)
-            
+
         query = request.GET.get('query', None)
 
         if query and query.lower() == 'only-parent':
@@ -59,7 +55,10 @@ class CategoryView(APIView):
             )
         elif query and query.lower() == 'only-feature':
             queryset = Category.objects.filter(
-                is_active=True, is_featured=True, is_deleted=False)
+                is_active=True,
+                is_featured=True,
+                is_deleted=False
+            )
         else:
             queryset = Category.objects.filter(
                 is_active=True, is_deleted=False)
@@ -71,11 +70,10 @@ class CategoryView(APIView):
             serializer = self.serializer_class(page, many=True)
 
             return paginator.get_paginated_response(serializer.data)
-        
+
         serializer = self.serializer_class(queryset, many=True)
 
         return Response(serializer.data)
-    
 
     @authentication_classes([SessionAuthentication, JWTAuthentication])
     @permission_classes([IsAuthenticated])
@@ -86,20 +84,23 @@ class CategoryView(APIView):
             return Response({
                 'error': 'Category ID is required to delete.'
             }, status=status.HTTP_400_BAD_REQUEST)
-        
+
         try:
-            category = Category.objects.get(pk=pk, is_active=True, is_deleted=False)
+            category = Category.objects.get(
+                pk=pk,
+                is_active=True,
+                is_deleted=False
+            )
             category.is_deleted = True
             category.save()
         except Category.DoesNotExist:
             return Response({
                 'error': 'Category does not exist.'
             }, status=status.HTTP_404_NOT_FOUND)
-        
+
         return Response({
             'message': 'Successfully deleted.',
         }, status=status.HTTP_202_ACCEPTED)
-    
 
     @authentication_classes([SessionAuthentication, JWTAuthentication])
     @permission_classes([IsAuthenticated])
@@ -110,7 +111,7 @@ class CategoryView(APIView):
             return Response({
                 'error': 'Category ID is required.',
             }, status=status.HTTP_400_BAD_REQUEST)
-        
+
         try:
             category = Category.objects.get(
                 pk=pk, is_active=True, is_deleted=False)
@@ -118,11 +119,14 @@ class CategoryView(APIView):
             return Response({
                 'error': 'Category does not exist.'
             }, status=status.HTTP_404_NOT_FOUND)
-        
-        serializer = CategorySerializer(category, data=request.data, partial=True)
+
+        serializer = CategorySerializer(
+            category,
+            data=request.data,
+            partial=True)
 
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
-        
+
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
