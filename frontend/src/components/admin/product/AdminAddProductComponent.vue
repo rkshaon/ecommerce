8eb5bfb3ae0ec920fdc5930197bdf8db6c8a872b
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-07-22 23:52:28 +0600

    frontend: admin - add product form

--- frontend/src/components/admin/product/AdminAddProductComponent.vue
+++ frontend/src/components/admin/product/AdminAddProductComponent.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="modal fade" id="addProductModal" tabindex="-1" aria-labelledby="addProductModalLabel"
-        aria-hidden="true" ref="addProductModal">
+    <div class="modal fade" id="addProductModal" tabindex="-1" aria-labelledby="addProductModalLabel" aria-hidden="true"
+        ref="addProductModal">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
@@ -11,21 +11,29 @@
                     <form @submit.prevent="createProduct">
                         <div class="mb-3">
                             <label for="title" class="form-label">Title</label>
-                            <input type="text" class="form-control" id="title" v-model="categoryForm.title">
+                            <input type="text" class="form-control" id="title" v-model="productForm.title">
                         </div>
                         <div class="mb-3">
-                            <label for="shortTitle" class="form-label">Short Title</label>
-                            <input type="text" class="form-control" id="shortTitle" v-model="categoryForm.short_title">
-                        </div>
-                        <div class="mb-3">
-                            <label for="icon" class="form-label">Icon</label>
-                            <input type="file" class="form-control" id="icon" @change="handleFileChangeOnInsert">
+                            <label for="category" class="form-label">Category</label>
+                            <select name="" class="form-control" id="category" v-model="productForm.category">
+                                <option :value="cat.id" v-for="(cat, index) in categoryList" :key="index">
+                                    {{ cat.title }}
+                                </option>
+                            </select>
                         </div>
                         <div class="mb-3">
                             <label for="description" class="form-label">Description</label>
-                            <textarea class="form-control" id="description" rows="3" v-model="categoryForm.description"
+                            <textarea class="form-control" id="description" rows="3" v-model="productForm.description"
                                 maxlength="255"></textarea>
                         </div>
+                        <!-- <div class="mb-3">
+                            <label for="shortTitle" class="form-label">Short Title</label>
+                            <input type="text" class="form-control" id="shortTitle" v-model="productForm.short_title">
+                        </div> -->
+                        <!-- <div class="mb-3">
+                            <label for="icon" class="form-label">Icon</label>
+                            <input type="file" class="form-control" id="icon" @change="handleFileChangeOnInsert">
+                        </div> -->
                         <button type="submit" class="btn btn-primary">Save</button>
                     </form>
                 </div>
@@ -36,25 +44,33 @@
 
 <script>
 import { Modal } from 'bootstrap';
-import { mapActions } from 'vuex';
+// import { mapActions } from 'vuex';
+import { mapState, mapActions } from 'vuex';
 import { useToast } from "vue-toastification";
 
 export default {
     name: 'AdminAddProductComponent',
     data() {
         return {
-            categoryForm: {
+            productForm: {
                 title: '',
-                short_title: '',
-                icon: null,
+                category: null,
                 description: '',
+                photos: [],
             }
         }
     },
+    computed: {
+        ...mapState('category', {
+            categoryList: 'categories'
+        }),
+    },
     methods: {
-        ...mapActions('category', ['addCategory']),
+        ...mapActions('category', ['fetchCategories']),
+        // ...mapActions('category', ['addCategory']),
 
         showAddProductModal() {
+            console.log(this.categoryList);
             const modalElement = this.$refs.addProductModal;
 
             if (modalElement) {
@@ -66,19 +82,20 @@ export default {
         },
 
         handleFileChangeOnInsert(event) {
-            this.categoryForm.icon = event.target.files[0];
+            this.productForm.icon = event.target.files[0];
         },
 
         async createProduct() {
+            console.log('Product: ', this.productForm);
             // const formData = new FormData();
 
-            // formData.append('title', this.categoryForm.title);
-            // formData.append('short_title', this.categoryForm.short_title);
-            // formData.append('description', this.categoryForm.description);
+            // formData.append('title', this.productForm.title);
+            // formData.append('short_title', this.productForm.short_title);
+            // formData.append('description', this.productForm.description);
             // formData.append('is_active', true);
 
-            // if (this.categoryForm.icon) {
-            //     formData.append('icon', this.categoryForm.icon);
+            // if (this.productForm.icon) {
+            //     formData.append('icon', this.productForm.icon);
             // }
             const toast = useToast();
             toast.warning('WIP');
@@ -92,9 +109,9 @@ export default {
             //     toast.error('Failed to create category.');
             // }
 
-            const modalElement = this.$refs.addProductModal;
-            const modal = new Modal(modalElement);
-            modal._hideModal();
+            // const modalElement = this.$refs.addProductModal;
+            // const modal = new Modal(modalElement);
+            // modal._hideModal();
         },
     },
 }
